40614b79b13a0f6451021be8e0978030828f3ccd
Arpita Guria <guriaarpita04@gmail.com>
2026-02-19 01:02:58 +0530

    commit change

--- js/main.js
+++ js/main.js
@@ -11,14 +11,8 @@ window.onscroll = function () {
     }
 }
 
-// nav hide 
-let navBar = document.querySelectorAll(".nav-link");
-let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function (a) {
-    a.addEventListener("click", function () {
-        navCollapse.classList.remove("show");
-    })
-})
+
+
 
 // Swiper Slider
 var swiper = new Swiper(".mySwiper", {
